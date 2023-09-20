c56ffcaa4fdf8b0c7f76137526110c79b3c9fb17
Md Ajbullah Mansuri <124435295+MdAjbullah@users.noreply.github.com>
2023-09-20 21:16:06 +0000

    bsic string concept

---
+++ .devcontainer/String/basic_sting.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class basic_sting {
+      public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the string");
+        String s=sc.nextLine();
+        System.out.println(s);
+
+    }
+    
+}
