527bf6b961a368eacaeb068a3d5daed421790b0b
Md Ajbullah Mansuri <124435295+MdAjbullah@users.noreply.github.com>
2023-09-20 21:13:27 +0000

    charAt function

---
+++ .devcontainer/String/charAt.java
@@ -0,0 +1,15 @@
+public class charAt {
+    public static void main(String[] args) {
+        String str = "Hello world!";
+        System.out.println(str.charAt(0));// charAt(int index) is the method to get the character at a specific index
+        System.out.println(str.charAt(1));
+        System.out.println(str.charAt(2));
+        char ch=str.charAt(6);//
+        System.out.println(ch);
+        System.out.println(str.length());// length() is the method to get the length of a string class
+
+
+
+    }
+    
+}
