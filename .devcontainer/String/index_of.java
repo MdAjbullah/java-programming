337c39fb2b017d08944478506a8f2de6b1f97333
Md Ajbullah Mansuri <124435295+MdAjbullah@users.noreply.github.com>
2023-09-20 21:14:23 +0000

    indexof Function

---
+++ .devcontainer/String/index_of.java
@@ -0,0 +1,14 @@
+public class index_of {
+   
+        public static void main(String[] args) {
+            String str = "abcde";
+            System.out.println(str.indexOf("c"));//indexOf() is a function to gate to index of string
+            System.out.println(str.indexOf("d"));//indexOf() is a function to gate to index of string
+            System.out.println(str.indexOf("e"));//indexOf() is a function to gate to index of string
+    
+        }
+    
+    
+    }
+    
+
