2940c9458f5cbfb0b357b3ee106bc7060d810cbc
Md Ajbullah Mansuri <124435295+MdAjbullah@users.noreply.github.com>
2023-09-21 17:44:20 +0000

    concat function

---
+++ .devcontainer/String/concat.java
@@ -0,0 +1,8 @@
+public class concat {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "World";
+        System.out.println(s1+s2); // .concat(s1, s2) are tow strings through concat function we can join both strings
+    }
+    
+}
