9b9f7362e8a48a4fe90b0659a996394b4025a99f
Md Ajbullah Mansuri <124435295+MdAjbullah@users.noreply.github.com>
2023-09-21 17:33:55 +0000

    StartWith function

---
+++ .devcontainer/String/contains.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class contains {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        String str="mdajbullah";
+
+        System.out.println(str.contains("m"));//contains  function find the given value is 
+                                                  //present or not if present hti we can get the output in true and false 
+
+    }
+    
+}
---
+++ .devcontainer/String/startWith.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class startWith {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("please enter String");
+        String str="mdajbullah";
+        System.out.println(str.endsWith("bull"));//endsWith("bull") is provide the end with the given initial string value is end with given
+        System.out.println(str.startsWith("md"));//startsWith("md") is provide the start with the given initial string value is start with given value
+    }
+
+}
