7f7c84451d2efdea0e7ae42bf7326f525cd663bb
Md Ajbullah Mansuri <124435295+MdAjbullah@users.noreply.github.com>
2023-09-24 17:35:52 +0000

    interning

---
+++ .devcontainer/String/Interning_New.java
@@ -0,0 +1,9 @@
+public class Interning_New {
+    public static void main(String[] args) {
+        String sc = "hellow";
+        String x= "heloow";
+        x="mellow";
+        System.out.println(x);
+
+    }
+}
