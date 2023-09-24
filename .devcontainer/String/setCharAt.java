ff6c2dcdb1550d55b97357eb12126117620de122
Md Ajbullah Mansuri <124435295+MdAjbullah@users.noreply.github.com>
2023-09-24 18:07:26 +0000

    setCharAt  function in StringBuilder function

---
+++ .devcontainer/String/setCharAt.java
@@ -0,0 +1,9 @@
+public class setCharAt {
+    public static void main(String[] args) {
+       StringBuilder str = new StringBuilder("hellow");
+       // hello -> hellow
+       str.setCharAt(0,'m');
+       System.out.println(str);
+    }
+    
+}
